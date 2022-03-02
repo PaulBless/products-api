e9ce804e842f6552ed8e1107bb49ab7a0bfcbd9b
PaulBless <eshunbless1@gmail.com>
2022-03-02 14:06:05 +0000

    added vue auth

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
 use Currency;
+use OpenExchangeRates;
+
 
 class ProductController extends Controller
 {
---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center align-items-center">
+        <div class="col-md-10">
+            <div class="card">
+                
+                <div class="card-header">{{ __('Dashboard') }}</div>
+                
+                
+                <a class='btn btn-info float-right mt-3 mb-3 ml-4 mr-4' href="">Add Product</a>
+                
+                
+                <div class="card-body">
+                    
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                    @endif
+                    
+                    
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
